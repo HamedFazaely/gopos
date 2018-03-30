34d25a46fe9a1342bd751fcfc9f9d6d5189bea7c
Unknown <hfazaely1984@gmail.com>
2018-03-31 01:23:53 +0430

    move const errors to ierrors package

--- iso/ierrors/errors.go
+++ iso/ierrors/errors.go
@@ -3,8 +3,12 @@ package ierrors
 import "errors"
 
 var (
-	ErrLeafCompNoChild   = errors.New("leaf field has no children")
-	ErrLeafCompNoBitmap  = errors.New("leaf field can not generate bitmap")
-	ErrNoMTIFieldPresent = errors.New("no MTI field present")
-	ErrNoneASCIIValue    = errors.New("only ASCII characters are allowed")
+	ErrLeafCompNoChild     = errors.New("leaf field has no children")
+	ErrLeafCompNoBitmap    = errors.New("leaf field can not generate bitmap")
+	ErrNoMTIFieldPresent   = errors.New("no MTI field present")
+	ErrNoneASCIIValue      = errors.New("only ASCII characters are allowed")
+	ErrLengthMissMatch     = errors.New("invalid input length")
+	ErrEncodingError       = errors.New("invalid input encoding")
+	ErrNotDigit            = errors.New("invalid numeric string")
+	ErrPackerNotCompatible = errors.New("packer and component are not compatible")
 )
